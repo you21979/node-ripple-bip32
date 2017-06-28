bac6a460311f6ebdcd8be8acaac7d743758be9d1
you21979 <you2197901@gmail.com>
2017-06-28 16:51:59 +0900

    add example

--- example/generate_address.js
+++ example/generate_address.js
@@ -1,11 +1,11 @@
 const bip39 = require("bip39");
-const HDNode = require("..");
+const bip32 = require("..");
 
 const mnemonic = bip39.generateMnemonic()
 console.log(mnemonic)
 
 const seed = bip39.mnemonicToSeed(mnemonic)
-const m = HDNode.fromSeedBuffer(seed)
+const m = bip32.fromSeedBuffer(seed)
 console.log(m.derivePath("m/44'/144'/0'").toBase58())
 console.log(m.derivePath("m/44'/144'/0'").neutered().toBase58())
 console.log(m.derivePath("m/44'/144'/0'/0/0").getAddress())
---
+++ example/transaction_sign.js
@@ -0,0 +1,31 @@
+const bip39 = require("bip39");
+const bip32 = require("..");
+const sign = require("ripple-sign-keypairs");
+const keypairs = require("ripple-keypairs");
+
+const mnemonic = bip39.generateMnemonic()
+console.log(mnemonic)
+const seed = bip39.mnemonicToSeed(mnemonic)
+const m = bip32.fromSeedBuffer(seed)
+console.log(m.derivePath("m/44'/144'/0'").toBase58())
+console.log(m.derivePath("m/44'/144'/0'").neutered().toBase58())
+console.log(m.derivePath("m/44'/144'/0'/0/0").getAddress())
+
+const srcpair = m.derivePath("m/44'/144'/0'/0/0").keyPair.getKeyPairs()
+srcpair.address = keypairs.deriveAddress(srcpair.publicKey)
+console.log(srcpair)
+
+const dest = keypairs.deriveKeypair(keypairs.generateSeed())
+dest.address = keypairs.deriveAddress(dest.publicKey)
+const tx = JSON.parse('{"TransactionType":"Payment","Account":"","Destination":"","Amount":"100000000","Flags":2147483648,"Fee":"100000","LastLedgerSequence":30828104,"Sequence":25}')
+tx.Account = srcpair.address
+tx.Destination = dest.address
+const txJSON = JSON.stringify(tx)
+const txSign = sign(txJSON, srcpair)
+console.log(txJSON)
+console.log(txSign)
+
+
+
+
+
