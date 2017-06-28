0fff56f920f9054cae53a8e4d50b4bc088793a70
you21979 <you2197901@gmail.com>
2017-06-28 16:52:54 +0900

    mod

--- example/transaction_sign.js
+++ example/transaction_sign.js
@@ -7,8 +7,6 @@ const mnemonic = bip39.generateMnemonic()
 console.log(mnemonic)
 const seed = bip39.mnemonicToSeed(mnemonic)
 const m = bip32.fromSeedBuffer(seed)
-console.log(m.derivePath("m/44'/144'/0'").toBase58())
-console.log(m.derivePath("m/44'/144'/0'").neutered().toBase58())
 console.log(m.derivePath("m/44'/144'/0'/0/0").getAddress())
 
 const srcpair = m.derivePath("m/44'/144'/0'/0/0").keyPair.getKeyPairs()
